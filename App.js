d1b8db4930fc0d0c8b8269f21ddd65c04885a260
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2024-12-13 10:28:24 +0700

    feat: slicing walled-mobile

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {flexDirection: 'column'}]}>
+      <StatusBar style="dark" />
+      <Image source={require('./assets/logo-lg.png')} style={{width: 200, height: 200, resizeMode: 'contain',}} />
+      <TextInput style={styles.input} placeholder='Email' keyboardType='email-address' />
+      <TextInput style={styles.input} placeholder='Password' secureTextEntry={true} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +18,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontWeight: '800',
+    backgroundColor: '#f2f2f2',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100,
+    padding: 35,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: '#f9f9f9',
+    fontSize: 16,
+  },
+  button: {
+    margin: 10,
+    backgroundColor: '#4DB6AC',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 15,
+    width: '100%',
+    alignItems: 'center',
+  },
+  loginText: {
+    fontWeight: 'bold',
+    color: "#fff",
   },
 });
